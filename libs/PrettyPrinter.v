bb56b37b54bb4de5ae358b9bee196c5e7bea5b67
vokor <volodyk.99@mail.ru>
2020-02-12 23:52:18 +0300

    pretty_list, pick_best_list created

--- libs/PrettyPrinter.v
+++ libs/PrettyPrinter.v
@@ -5,43 +5,27 @@ Require Import ZArith Int.
 Require Import Coq.Lists.List.
 Require Import String.
 
-Definition is_better_than (a:t) (b:t) :=
-  match a.(height) ?= b.(height) with
-  | Lt => a
-  | Eq => match total_width a ?= total_width b with
-          | Lt => a
-          | Eq => if (is_less_than b a)
-                  then b
-                  else a
-          | Gt => b
-          end
-  | Gt => b
-  end.
-
-(* Pick with minimum height *)
-(* FIXME: add <> nil *)
-Definition pick_best (lst: list t):=
+Fixpoint get_min_height (lst : list t) (w: nat) (res: option nat) :=
   match lst with
-  | nil => empty
-  | hd :: tl => fold_left is_better_than tl hd
+  | nil => res
+  | hd :: tl => if total_width hd <=? w
+                then match res with
+                     | Some n => get_min_height tl w (Some (min n hd.(height)))
+                     | None => get_min_height tl w (Some hd.(height))
+                     end
+                else get_min_height tl w res
   end.
-
-(*
-Definition pick_best (lst : {lst : list t | nil <> lst}) :=
+                    
+Definition pick_best_list (lst : list t) (w: nat) :=
   match lst with
-  | exist _ x _ =>
-    match x with
-    | nil => empty
-    | hd :: tl => fold_right is_better_than hd tl
-    end
-  end.
-*)
-
-Program Definition best_to_str (lst: list t) :=
-  match lst with
-  | nil => "Empty set of strings to choose from."%string
-  | _ => to_string (pick_best lst)
+  | nil => nil
+  | _   =>  match get_min_height lst w None with
+            | None   => nil
+            | Some n => filter
+                          (fun f => andb (total_width f <=? w) (f.(height) =? n))
+                          lst
+            end
   end.
 
-Definition pretty (docToVar: nat -> Doc -> list t) (width: nat) (doc: Doc): string :=
-  best_to_str (docToVar width doc).
+Definition pretty_list (docToVar: nat -> Doc -> list t) (width: nat) (doc: Doc) :=
+  pick_best_list (docToVar width doc) width.
